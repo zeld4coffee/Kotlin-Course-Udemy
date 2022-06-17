da97978619550d99ed33e46558dd060375ae7413
Ailton cesar <ailtonc.n64@gmail.com>
2022-06-17 16:57:38 -0300

    prints

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // impressão dos dados
+    println("Print 1")
+    print("Print 2")
+
+
 }
